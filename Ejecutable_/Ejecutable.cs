6003e1eb8a791bb778775fda79e7e5782f161e79
ManuelAlejandro <manuelalejandrogamboa35@gmail.com>
2023-11-09 01:44:30 -0500

    municipios(provincias)

--- Ejecutable_/Ejecutable.cs
+++ Ejecutable_/Ejecutable.cs
@@ -16,33 +16,33 @@ namespace Ejecutable_
 
             List<Entity> entityList = new List<Entity>();
 
-            List<(string, List<double>)> data = proveedor.GetParamEP_EAM_EAJ_EMF();
+            List<(string, List<double>, string)> data = proveedor.GetParamEP_EAM_EAJ_EMF();
 
             foreach (var item in data)
             {
-                Entity entity = new Entity(item.Item2, item.Item1);
+                Entity entity = new Entity(item.Item2, new List<string>() { item.Item1, item.Item3});
                 entityList.Add(entity);
             }
 
             Algoritm algoritm = new Algoritm();
             K_Means K_Means = new K_Means();
 
-            List<List<Entity>> result = algoritm.Clusters(new List<List<Entity>>() { entityList }, 3);
+           // List<List<Entity>> result = algoritm.Clusters(new List<List<Entity>>() { entityList }, 3);
 
             (List<List<Entity>>, List<double>[]) resultKM = K_Means.KMeans(new List<List<Entity>>() { entityList }, 5);
 
-            Console.WriteLine( "Minimiazar distancias maxima");
-            Console.WriteLine( );
-            foreach (var entityGroup in result)
-            {
-                Console.WriteLine($"Cluster {result.IndexOf(entityGroup)}");
-                foreach (var entity in entityGroup)
-                {
-                    Console.WriteLine( $"{entity} ");
-                }
+            //Console.WriteLine( "Minimiazar distancias maxima");
+            //Console.WriteLine( );
+            //foreach (var entityGroup in result)
+            //{
+            //    Console.WriteLine($"Cluster {result.IndexOf(entityGroup)}");
+            //    foreach (var entity in entityGroup)
+            //    {
+            //        Console.WriteLine( $"{entity} ");
+            //    }
 
-                Console.WriteLine( );
-            }
+            //    Console.WriteLine( );
+            //}
 
             Console.WriteLine("K-Means");
             Console.WriteLine( );
@@ -51,9 +51,9 @@ namespace Ejecutable_
                 Console.WriteLine($"Cluster {i} -> {ImprimeCoordenadas(resultKM.Item2[i])}");
                 for (int j = 0; j < resultKM.Item1[i].Count; j++)
                 {
-                    Console.WriteLine(resultKM.Item1[i][j]);
+                    Console.WriteLine(resultKM.Item1[i][j] + $"({resultKM.Item1[i][j].caracteristics[1]})");
                 }
-                Console.WriteLine(  );
+                Console.WriteLine();
             }
         }
 
